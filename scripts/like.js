ff8542f4f51c22126e6fa174b55ee096c8923f11
magog <maksim.25705@gmail.com>
2025-12-14 21:51:17 +0300

    more changes js

--- scripts/like.js
+++ scripts/like.js
@@ -2,7 +2,6 @@
 ✦ like-icon — для svg-иконки анимированного сердца
 ✦ card__like-button — для кнопки Like рядом с иконкой
 ✦ card__icon-button — для кнопки, оборачивающей иконку
-✦ card__icon-button — для кнопки, оборачивающей иконку
 ✦ is-liked — для обозначения состояния лайкнутой иконки в виде сердца
 ✦ button__text — для обозначения текстового элемента внутри кнопки
 Если эти классы поменять в HTML, скрипт перестанет работать. Будьте аккуратны.
@@ -13,12 +12,17 @@ const likeButtonArray = document.querySelectorAll('.card__like-button');
 const iconButtonArray = document.querySelectorAll('.card__icon-button');
 
 iconButtonArray.forEach((iconButton, index) => {
-  iconButton.onclick = () =>
+  iconButton.addEventListener('click', (event) => {
+    event.preventDefault();
     toggleIsLiked(likeHeartArray[index], likeButtonArray[index]);
+  });
 });
 
 likeButtonArray.forEach((button, index) => {
-  button.onclick = () => toggleIsLiked(likeHeartArray[index], button);
+  button.addEventListener('click', (event) => {
+    event.preventDefault();
+    toggleIsLiked(likeHeartArray[index], button);
+  });
 });
 
 function toggleIsLiked(heart, button) {
@@ -40,6 +44,7 @@ function setButtonText(heart, button) {
   }
 }
 
+const popup = document.getElementById('popup-id');
 const popupOpenButton = document.querySelector('.button__popup-open');
 const popupCloseButton = document.querySelector('.button__popup-close');
 
